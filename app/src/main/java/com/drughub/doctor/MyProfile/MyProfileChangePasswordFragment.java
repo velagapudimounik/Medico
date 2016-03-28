a513b6df0a22d6f5f3057949bbf340e29e51b318
Umadevi Vanga <umadevi.vanga@drughub.in>
2016-03-28 10:30:41 +0530

    Implemented my profile screens.

---
+++ app/src/main/java/com/drughub/doctor/MyProfile/MyProfileChangePasswordFragment.java
@@ -0,0 +1,19 @@
+package com.drughub.doctor.MyProfile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.drughub.doctor.R;
+
+public class MyProfileChangePasswordFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getActivity().setTitle("My Profile");
+        return inflater.inflate(R.layout.myprofile_changepassword_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/com/drughub/doctor/MyProfile/MyProfileEditClinicDailogBox.java
@@ -0,0 +1,19 @@
+package com.drughub.doctor.MyProfile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.drughub.doctor.R;
+
+public class MyProfileEditClinicDailogBox extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getActivity().setTitle("My Profile");
+        return inflater.inflate(R.layout.myprofile_editclinic_dailogbox,container,false);
+    }
+}
