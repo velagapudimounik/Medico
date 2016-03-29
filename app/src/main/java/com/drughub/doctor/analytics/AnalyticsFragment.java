0f3d079e410f4fbc095f8037dd2be93b0d39cda6
Deepak <deepak.pandiri@drughub.in>
2016-03-29 18:22:44 +0530

    Charts in Analytics

--- app/src/main/java/com/drughub/doctor/analytics/AnalyticsFragment.java
+++ app/src/main/java/com/drughub/doctor/analytics/AnalyticsFragment.java
@@ -11,16 +11,34 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.drughub.doctor.R;
 import com.drughub.doctor.utils.DrughubIcon;
+import com.drughub.doctor.utils.GaugeView;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnalyticsFragment extends Fragment implements RadioGroup.OnCheckedChangeListener {
     RadioButton radioConsumption, radioFMP, radioProfitability, radioColdHub;
     RadioButton radioWeekly, radioMonthly, radioYearly;
     TextView title;
     LinearLayout coldHubLayout, restLayout;
+    BarChart chart;
+    HorizontalBarChart horizontalBarChart;
+    private ScrollView pieChartLayout;
+    private GaugeView chartPie1;
+    private GaugeView chartPie2;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,6 +49,14 @@ public class AnalyticsFragment extends Fragment implements RadioGroup.OnCheckedC
         title = (TextView) view.findViewById(R.id.title);
         restLayout = (LinearLayout) view.findViewById(R.id.restLayout);
         coldHubLayout = (LinearLayout) view.findViewById(R.id.coldhubLayout);
+        chart = (BarChart) view.findViewById(R.id.chart);
+        horizontalBarChart = (HorizontalBarChart) view.findViewById(R.id.chart_horizontal);
+
+        pieChartLayout = (ScrollView) view.findViewById(R.id.pie_chart_layout);
+        chartPie1 = (GaugeView) view.findViewById(R.id.chart_pie1);
+        chartPie2 = (GaugeView) view.findViewById(R.id.chart_pie2);
+
+        chartInit(getXAxisValues("week"), getDataSet("week"));
 
         RadioGroup analyticsGroup = (RadioGroup) view.findViewById(R.id.analyticsGroup);
         RadioGroup forecastGroup = (RadioGroup) view.findViewById(R.id.forecastGroup);
@@ -57,6 +83,55 @@ public class AnalyticsFragment extends Fragment implements RadioGroup.OnCheckedC
         return view;
     }
 
+    private void chartInit(List<String> XAxisValues, IBarDataSet dataset) {
+        BarData data = new BarData(XAxisValues, dataset);
+        chart.setData(data);
+        chart.setDescription(null);
+        chart.animateXY(2000, 2000);
+
+        chart.getData().setDrawValues(false);
+        chart.setPinchZoom(true);
+        chart.setScaleEnabled(true);
+        chart.setDoubleTapToZoomEnabled(false);
+        chart.setHighlightPerDragEnabled(false);
+        chart.setHighlightPerTapEnabled(false);
+
+        chart.getXAxis().setDrawGridLines(false);
+        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        chart.getXAxis().setDrawAxisLine(false);
+        chart.getXAxis().setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+        chart.invalidate();
+    }
+
+    private void horizontalChartInit(HorizontalBarChart horizontalBarChart, List<String> XAxisValues, IBarDataSet dataset) {
+        BarData data = new BarData(XAxisValues, dataset);
+        horizontalBarChart.setData(data);
+        horizontalBarChart.setDescription(null);
+        horizontalBarChart.animateXY(2000, 2000);
+
+        horizontalBarChart.getData().setDrawValues(false);
+        horizontalBarChart.setPinchZoom(true);
+        horizontalBarChart.setScaleEnabled(true);
+        horizontalBarChart.setDoubleTapToZoomEnabled(false);
+        horizontalBarChart.setHighlightPerDragEnabled(false);
+        horizontalBarChart.setHighlightPerTapEnabled(false);
+
+        horizontalBarChart.getXAxis().setDrawGridLines(false);
+        horizontalBarChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        horizontalBarChart.getXAxis().setDrawAxisLine(false);
+        horizontalBarChart.getXAxis().setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+        horizontalBarChart.getAxisLeft().setEnabled(false);
+        horizontalBarChart.getAxisRight().setEnabled(false);
+
+        horizontalBarChart.getLegend().setEnabled(false);
+
+        horizontalBarChart.invalidate();
+    }
+
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         getActivity().setTitle(getString(R.string.analytics));
@@ -70,20 +145,38 @@ public class AnalyticsFragment extends Fragment implements RadioGroup.OnCheckedC
                 case R.id.radioButton1: // Consumption
                     radioConsumption.setText(getText(R.string.icon_consumption_s));
                     title.setText(getString(R.string.consumption));
+                    chart.setVisibility(View.VISIBLE);
+                    horizontalBarChart.setVisibility(View.GONE);
+                    pieChartLayout.setVisibility(View.GONE);
                     break;
                 case R.id.radioButton2: // Fast-Moving Products
                     radioFMP.setText(getText(R.string.icon_fmp_s));
                     title.setText(getString(R.string.fmp));
+                    chart.setVisibility(View.GONE);
+                    horizontalBarChart.setVisibility(View.VISIBLE);
+                    pieChartLayout.setVisibility(View.GONE);
+                    horizontalChartInit(horizontalBarChart, getXAxisValues("week"), getDataSet("week"));
                     break;
                 case R.id.radioButton3: // Profitability
                     radioProfitability.setText(getText(R.string.icon_profitability_s));
                     title.setText(getString(R.string.profitability));
+                    chart.setVisibility(View.VISIBLE);
+                    horizontalBarChart.setVisibility(View.GONE);
+                    pieChartLayout.setVisibility(View.GONE);
                     break;
                 case R.id.radioButton4: // ColdHub
                     radioColdHub.setText(getText(R.string.icon_coldhub_s));
                     title.setText(getString(R.string.coldhub));
                     restLayout.setVisibility(View.GONE);
                     coldHubLayout.setVisibility(View.VISIBLE);
+                    chart.setVisibility(View.GONE);
+                    horizontalBarChart.setVisibility(View.GONE);
+                    pieChartLayout.setVisibility(View.VISIBLE);
+                    chartPie1.setTargetValue(50);
+                    chartPie2.setTargetValue(70);
+
+//                    horizontalBarChart2.setMaxVisibleValueCount(100);
+//                    horizontalChartInit(horizontalBarChart2, getXAxisValues(""), getDataSet(""));
                     break;
                 default:
                     break;
@@ -101,21 +194,31 @@ public class AnalyticsFragment extends Fragment implements RadioGroup.OnCheckedC
                 case R.id.radioWeekly:
                     radioWeekly.setBackgroundResource(R.drawable.fill_rect_orange);
                     radioWeekly.setTextColor(Color.WHITE);
+                    changeCharts("week");
+
                     break;
                 case R.id.radioMonthly:
                     radioMonthly.setBackgroundResource(R.drawable.fill_rect_orange);
                     radioMonthly.setTextColor(Color.WHITE);
+                    changeCharts("month");
                     break;
                 case R.id.radioYearly:
                     radioYearly.setBackgroundResource(R.drawable.fill_rect_orange);
                     radioYearly.setTextColor(Color.WHITE);
+                    changeCharts("year");
                     break;
                 default:
                     break;
             }
-
         }
+    }
 
+    private void changeCharts(String type) {
+        if (chart.getVisibility() == View.VISIBLE) {
+            chartInit(getXAxisValues(type), getDataSet(type));
+        } else if (horizontalBarChart.getVisibility() == View.VISIBLE) {
+            horizontalChartInit(horizontalBarChart, getXAxisValues(type), getDataSet(type));
+        }
     }
 
     private void resetButtons() {
@@ -129,4 +232,65 @@ public class AnalyticsFragment extends Fragment implements RadioGroup.OnCheckedC
         radioWeekly.setChecked(true);
 
     }
+
+    private IBarDataSet getDataSet(String type) {
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        if (type.equals("week")) {
+            for (int i = 0; i < 7; i++) {
+                valueSet1.add(new BarEntry(((i + 1) * 10), i));
+            }
+        } else if (type.equals("month")) {
+            for (int i = 0; i < 12; i++) {
+                valueSet1.add(new BarEntry(((i + 1) * 10), i));
+            }
+        } else if (type.equals("year")) {
+            for (int i = 0; i < 4; i++) {
+                valueSet1.add(new BarEntry(((i + 1) * 10), i));
+            }
+        } else {
+            valueSet1.add(new BarEntry(78, 0));
+        }
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
+        barDataSet1.setColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+
+        return barDataSet1;
+    }
+
+    private List<String> getXAxisValues(String type) {
+        ArrayList<String> xAxis = new ArrayList<>();
+        if (type.equals("week")) {
+            xAxis.add("Mon");
+            xAxis.add("Tue");
+            xAxis.add("Wed");
+            xAxis.add("Thu");
+            xAxis.add("Fri");
+            xAxis.add("Sat");
+            xAxis.add("Sun");
+        } else if (type.equals("month")) {
+            xAxis.add("Jan");
+            xAxis.add("Feb");
+            xAxis.add("Mar");
+            xAxis.add("Apr");
+            xAxis.add("May");
+            xAxis.add("Jun");
+            xAxis.add("Jul");
+            xAxis.add("Aug");
+            xAxis.add("Sep");
+            xAxis.add("Oct");
+            xAxis.add("Nov");
+            xAxis.add("Dec");
+        } else if (type.equals("year")) {
+            xAxis.add("2013");
+            xAxis.add("2014");
+            xAxis.add("2015");
+            xAxis.add("2016");
+        } else {
+            xAxis.add("");
+        }
+        return xAxis;
+    }
+
+
 }
