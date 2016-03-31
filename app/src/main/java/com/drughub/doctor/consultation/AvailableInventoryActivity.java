b904b0d09cc1c48e5a3302420b48171db50e7026
Deepak <deepak.pandiri@drughub.in>
2016-03-31 15:08:25 +0530

    Added line chart in Consultations disease expands.

--- app/src/main/java/com/drughub/doctor/consultation/AvailableInventoryActivity.java
+++ app/src/main/java/com/drughub/doctor/consultation/AvailableInventoryActivity.java
@@ -1,6 +1,7 @@
 package com.drughub.doctor.consultation;
 
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,17 +16,23 @@ import com.drughub.doctor.BaseActivity;
 import com.drughub.doctor.R;
 import com.drughub.doctor.utils.CustomDialog;
 import com.drughub.doctor.utils.StringUtils;
-
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-class AvailableInventoryItem
-{
+class AvailableInventoryItem {
     public String vaccineName;
     public String skuID;
     public String mfrName;
-    public AvailableInventoryItem(String vaccine, String sku, String mfr)
-    {
+
+    public AvailableInventoryItem(String vaccine, String sku, String mfr) {
         this.vaccineName = vaccine;
         this.skuID = sku;
         this.mfrName = mfr;
@@ -49,14 +56,13 @@ public class AvailableInventoryActivity extends BaseActivity {
 
         //mRecyclerView.addItemDecoration(new SimpleDividerItemDecoration(getActivity()));
 
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this ,LinearLayoutManager.VERTICAL, false);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         ArrayList<AvailableInventoryItem> mDataSet = new ArrayList<>();
 
-        for(int i=0; i<10; i++)
-        {
-            AvailableInventoryItem item = new AvailableInventoryItem("Vaccine Name"+i, "1234567890", "MFR Name");
+        for (int i = 0; i < 10; i++) {
+            AvailableInventoryItem item = new AvailableInventoryItem("Vaccine Name" + i, "1234567890", "MFR Name");
             mDataSet.add(item);
         }
 
@@ -64,8 +70,7 @@ public class AvailableInventoryActivity extends BaseActivity {
         mRecyclerView.setAdapter(mAdapter);
     }
 
-    public static class AvailableInventoryListAdapter extends RecyclerView.Adapter<AvailableInventoryListAdapter.ViewHolder>
-    {
+    public static class AvailableInventoryListAdapter extends RecyclerView.Adapter<AvailableInventoryListAdapter.ViewHolder> {
         private ArrayList<AvailableInventoryItem> mDataSet;
         static FragmentActivity sContext;
 
@@ -73,10 +78,9 @@ public class AvailableInventoryActivity extends BaseActivity {
             private final TextView vaccineName;
             private View mItemView;
             View expandBtn;
-            View moreInfo;
+            LineChart lineChart;
 
-            public ViewHolder(View v)
-            {
+            public ViewHolder(View v) {
                 super(v);
 
                 mItemView = v;
@@ -84,13 +88,12 @@ public class AvailableInventoryActivity extends BaseActivity {
                 View vaccinateBtn = v.findViewById(R.id.vaccinateBtn);
                 vaccinateBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View v)
-                    {
+                    public void onClick(View v) {
                         String value = "4";
 
-                        final Dialog dialog = CustomDialog.showMessageDialog((BaseActivity)sContext,
+                        final Dialog dialog = CustomDialog.showMessageDialog((BaseActivity) sContext,
                                 StringUtils.findAndReplace(sContext.getResources().getString(R.string.temperature), "{temp}", value), "Done");
-                        Button okBtn = (Button)dialog.findViewById(R.id.dialogOkBtn);
+                        Button okBtn = (Button) dialog.findViewById(R.id.dialogOkBtn);
                         okBtn.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -105,35 +108,34 @@ public class AvailableInventoryActivity extends BaseActivity {
                 expandBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (moreInfo.getVisibility() != View.VISIBLE) {
-                            moreInfo.setVisibility(View.VISIBLE);
+                        if (lineChart.getVisibility() != View.VISIBLE) {
+                            lineChart.setVisibility(View.VISIBLE);
+                            setData(lineChart, 6, 5);
                             expandBtn.setRotation(180);
                         } else {
-                            moreInfo.setVisibility(View.GONE);
+                            lineChart.setVisibility(View.GONE);
                             expandBtn.setRotation(0);
                         }
                     }
                 });
 
-                moreInfo = v.findViewById(R.id.moreInfo);
+                lineChart = (LineChart) v.findViewById(R.id.moreInfo);
+
             }
 
-            public void setItemSelected(boolean selected)
-            {
+            public void setItemSelected(boolean selected) {
                 mItemView.setSelected(selected);
             }
 
-            public void setItemDetails(AvailableInventoryItem item)
-            {
+            public void setItemDetails(AvailableInventoryItem item) {
                 vaccineName.setText(item.vaccineName);
 
-                moreInfo.setVisibility(View.GONE);
+                lineChart.setVisibility(View.GONE);
                 expandBtn.setRotation(0);
             }
         }
 
-        public AvailableInventoryListAdapter(ArrayList<AvailableInventoryItem> dataSet, FragmentActivity context)
-        {
+        public AvailableInventoryListAdapter(ArrayList<AvailableInventoryItem> dataSet, FragmentActivity context) {
             mDataSet = dataSet;
             sContext = context;
         }
@@ -148,18 +150,87 @@ public class AvailableInventoryActivity extends BaseActivity {
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder viewHolder, final int position)
-        {
+        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
             // Get element from your dataset at this position and replace the contents of the view
             // with that element
             viewHolder.setItemDetails(mDataSet.get(position));
         }
 
         @Override
-        public int getItemCount()
-        {
+        public int getItemCount() {
             return mDataSet.size();
         }
     }
 
+    public static void setData(LineChart lineChart, int count, float range) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("12 Oct");
+        xVals.add("13 Oct");
+        xVals.add("14 Oct");
+        xVals.add("15 Oct");
+        xVals.add("16 Oct");
+        xVals.add("17 Oct");
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            yVals1.add(new Entry(i * 2, i));
+        }
+        for (int i = 0; i < count; i++) {
+            yVals2.add(new Entry(3, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals1, "Manufacturer");
+        LineDataSet set2 = new LineDataSet(yVals2, "Transit");
+        LineDataSet set3 = new LineDataSet(yVals1, "Warehouse");
+        LineDataSet set4 = new LineDataSet(yVals2, "Transit");
+        LineDataSet set5 = new LineDataSet(yVals1, "Doctor");
+
+
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setCircleColor(Color.WHITE);
+        set1.setLineWidth(2f);
+        set1.setCircleRadius(3f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setDrawCircleHole(false);
+        //set1.setFillFormatter(new MyFillFormatter(0f));
+//        set1.setDrawHorizontalHighlightIndicator(false);
+//        set1.setVisible(false);
+//        set1.setCircleHoleColor(Color.WHITE);
+
+        // create a dataset and give it a type
+        set2.setAxisDependency(YAxis.AxisDependency.RIGHT);
+        set2.setColor(Color.RED);
+        set2.setCircleColor(Color.WHITE);
+        set2.setLineWidth(2f);
+        set2.setCircleRadius(3f);
+        set2.setFillAlpha(65);
+        set2.setFillColor(Color.RED);
+        set2.setDrawCircleHole(false);
+        set2.setHighLightColor(Color.rgb(244, 117, 117));
+        //set2.setFillFormatter(new MyFillFormatter(900f));
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+//        dataSets.add(set5);
+//        dataSets.add(set4);
+//        dataSets.add(set3);
+//        dataSets.add(set2);
+        dataSets.add(set1);
+        // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+        data.setValueTextColor(Color.TRANSPARENT);
+        data.setValueTextSize(9f);
+
+        // set data
+        lineChart.setData(data);
+    }
 }
