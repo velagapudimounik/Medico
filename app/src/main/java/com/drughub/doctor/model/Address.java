671a3e16a36e6dab175662df78f4a9a9007511ac
Umadevi Vanga <umadevi.vanga@drughub.in>
2016-04-04 13:13:56 +0530

    Address getters and setters

---
+++ app/src/main/java/com/drughub/doctor/model/Address.java
@@ -0,0 +1,155 @@
+package com.drughub.doctor.model;
+
+import io.realm.RealmObject;
+
+public class Address extends RealmObject {
+    private String buildingName;
+    private String Country;
+    private String colony;
+    private String City;
+    private String street;
+    private String State;
+    private String doorNumber;
+    private Integer postalCode;
+    private String addressType;
+    private String district;
+    private String tinno;
+    private String companyName;
+    private String colonyName;
+    private String streetName;
+    private double lng;
+    private double lat;
+    private Integer dlno;
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+    public String getColony() {
+        return colony;
+    }
+
+    public void setColony(String colony) {
+        this.colony = colony;
+    }
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public String getDoorNumber() {
+        return doorNumber;
+    }
+
+    public void setDoorNumber(String doorNumber) {
+        this.doorNumber = doorNumber;
+    }
+
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(String addressType) {
+        this.addressType = addressType;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getTinno() {
+        return tinno;
+    }
+
+    public void setTinno(String tinno) {
+        this.tinno = tinno;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getColonyName() {
+        return colonyName;
+    }
+
+    public void setColonyName(String colonyName) {
+        this.colonyName = colonyName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public Integer getDlno() {
+        return dlno;
+    }
+
+    public void setDlno(Integer dlno) {
+        this.dlno = dlno;
+    }
+}
