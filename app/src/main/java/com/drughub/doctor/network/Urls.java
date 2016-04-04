4b1695fdef44e5bb086c603460e3de78f31c218e
Umadevi Vanga <umadevi.vanga@drughub.in>
2016-04-04 17:44:45 +0530

    Add Clinic,Update clinic methods for urls

--- app/src/main/java/com/drughub/doctor/network/Urls.java
+++ app/src/main/java/com/drughub/doctor/network/Urls.java
@@ -4,7 +4,8 @@ public class Urls {
     public static String BASE_URL = "";
     public static String SIGN_UP = BASE_URL+"/serviceprovider/1.0/signup";
     public static String SIGN_IN = BASE_URL+"/1.0/login/loginAuthentication";
-
+    public static String ADD_CLINIC=BASE_URL+"/1.0/sp/clinics/{userId}";
+    public static String UPDATE_CLINIC=BASE_URL+"/1.0/sp/clinic/{userId}";
     public static String CREATE_CALANDER = BASE_URL+"/1.0/sp/clinic";
     public static String SERVICE_PROVIDER = BASE_URL+"/1.0/sp/clinic";
 }
