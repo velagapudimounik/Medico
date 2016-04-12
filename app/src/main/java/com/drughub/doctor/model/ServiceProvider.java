731cfb38f2a80ef46f32bd71e098facaa49078bb
Deepak <deepak.pandiri@drughub.in>
2016-04-12 12:31:45 +0530

    ServiceProvider Model

--- app/src/main/java/com/drughub/doctor/model/ServiceProvider.java
+++ app/src/main/java/com/drughub/doctor/model/ServiceProvider.java
@@ -7,29 +7,26 @@ import com.drughub.doctor.network.Urls;
 
 import org.json.JSONObject;
 
-import java.util.HashMap;
-
+import io.realm.RealmList;
 import io.realm.RealmObject;
 
 
 public class ServiceProvider extends RealmObject {
 
-    private String firstName = "Achyuth";
-    private String lastName = "Kumar";
-    private String phone = "123456789";
-    private String buildingName;
-    private String colony;
-    private String doorNumber;
-    private String street;
-    private String postalCode;
-    private String City = "HYd";
-    private String State;
-    private String Country = "India";
-    private String email = "achyuth@drughub.in";
-    private String qualification = "Btech";
-    private String specialization = "Android";
-    private Integer experienceInYears = 1;
-    private Address address = new Address();
+    private ValueIds gender;
+    private String mobile = "123456789";
+    private int spProfileId = 12;
+    private String emailId = "achyuth@drughub.in";
+    private RealmList<ValueIds> qualificationList;
+    private RealmList<ValueIds> specializationList;
+    private String practiseStartDate;
+    private String partnerNature;
+    private String profileName;
+    private String profileDescription;
+    private String middleName;
+    private String lastName;
+    private String firstName;
+    private Address address;
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
@@ -48,133 +45,16 @@ public class ServiceProvider extends RealmObject {
         this.lastName = lastName;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getBuildingName() {
-        return buildingName;
-    }
-
-    public void setBuildingName(String buildingName) {
-        this.buildingName = buildingName;
-    }
-
-    public String getColony() {
-        return colony;
-    }
-
-    public void setColony(String colony) {
-        this.colony = colony;
-    }
-
-    public String getDoorNumber() {
-        return doorNumber;
-    }
-
-    public void setDoorNumber(String doorNumber) {
-        this.doorNumber = doorNumber;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getCity() {
-        return City;
-    }
-
-    public void setCity(String city) {
-        City = city;
-    }
-
-    public String getState() {
-        return State;
-    }
-
-    public void setState(String state) {
-        State = state;
-    }
-
-    public String getCountry() {
-        return Country;
-    }
-
-    public void setCountry(String contry) {
-        Country = contry;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getQualification() {
-        return qualification;
-    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    public Integer getExperienceInYears() {
-        return experienceInYears;
-    }
-
-    public void setExperienceInYears(Integer experienceInYears) {
-        this.experienceInYears = experienceInYears;
-    }
-
     public String toUpdateServiceProvider() {
         JSONObject object = new JSONObject();
-        JSONObject address_object = new JSONObject();
         try {
             object.put("firstName", getFirstName());
             object.put("lastname", getLastName());
-            object.put("phone", getPhone());
-            object.put("qualification", getQualification());
-            object.put("specialization", getSpecialization());
-            object.put("experienceInYears", getExperienceInYears());
-
-            address_object.put("buildingName", address.getBuildingName());
-            address_object.put("country", address.getCountry());
-            address_object.put("colony", address.getColony());
-            address_object.put("doorNumber", address.getDoorNumber());
-            address_object.put("city", address.getCity());
-            address_object.put("street", address.getStreet());
-            address_object.put("postalCode", address.getPostalCode());
-            address_object.put("state", address.getState());
-
-
-            object.put("address", address_object);
-
+            object.put("mobile", getMobile());
+//            object.put("qualification", getQualification());
+//            object.put("specialization", getSpecialization());
+//            object.put("experienceInYears", getExperienceInYears());
+            object.put("address", address.toServiceProvider());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -182,32 +62,23 @@ public class ServiceProvider extends RealmObject {
     }
 
 
-    public void UpdateServiceProvider(Context context) {
-        HashMap<String, String> headers = new HashMap<>();
-        HashMap<String, String> params = new HashMap<>();
-        Globals.PUT(Urls.SERVICE_PROVIDER, headers, params, toUpdateServiceProvider(), new Globals.VolleyCallback() {
+    public void UpdateServiceProvider(Context context, final Globals.VolleyCallback callback) {
+        Globals.PUT(Urls.SERVICE_PROVIDER, null, null, toUpdateServiceProvider(), new Globals.VolleyCallback() {
             @Override
             public void onSuccess(String result) {
-                try {
-                    JSONObject response = new JSONObject();
-                    // ToDo UpdateServiceProvider(SP)
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                callback.onSuccess(result);
             }
 
             @Override
             public void onFail(String result) {
-
+                callback.onFail(result);
             }
         });
 
     }
 
-    public void CreateServiceProvider() {
-        HashMap<String, String> headers = new HashMap<>();
-        Globals.GET(Urls.SERVICE_PROVIDER, headers, new Globals.VolleyCallback() {
+    public void GetServiceProvider() {
+        Globals.GET(Urls.SERVICE_PROVIDER, null, new Globals.VolleyCallback() {
             @Override
             public void onSuccess(String result) {
 
@@ -218,7 +89,101 @@ public class ServiceProvider extends RealmObject {
 
             }
         });
+    }
 
+    public String getMiddleName() {
+        return middleName;
+    }
 
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public int getSpProfileId() {
+        return spProfileId;
+    }
+
+    public void setSpProfileId(int spProfileId) {
+        this.spProfileId = spProfileId;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public ValueIds getGender() {
+        return gender;
+    }
+
+    public void setGender(ValueIds gender) {
+        this.gender = gender;
+    }
+
+    public RealmList<ValueIds> getQualificationList() {
+        return qualificationList;
+    }
+
+    public void setQualificationList(RealmList<ValueIds> qualificationList) {
+        this.qualificationList = qualificationList;
+    }
+
+    public RealmList<ValueIds> getSpecializationList() {
+        return specializationList;
+    }
+
+    public void setSpecializationList(RealmList<ValueIds> specializationList) {
+        this.specializationList = specializationList;
+    }
+
+    public String getPractiseStartDate() {
+        return practiseStartDate;
+    }
+
+    public void setPractiseStartDate(String practiseStartDate) {
+        this.practiseStartDate = practiseStartDate;
+    }
+
+    public String getPartnerNature() {
+        return partnerNature;
+    }
+
+    public void setPartnerNature(String partnerNature) {
+        this.partnerNature = partnerNature;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 }
